70d5c445644c4d01cab3d49f03bdd7689ecac238
Krish Maha <krm221@aube.doc.ic.ac.uk>
2022-11-05 17:09:17 +0000

    Add syscall stubs

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,9 +1,12 @@
-#include "userprog/syscall.h"
 #include <stdio.h>
+#include <stdbool.h>
+
+#include "userprog/syscall.h"
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "devices/shutdown.h"
+#include "userprog/process.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -30,4 +33,52 @@ syscall_handler (struct intr_frame *f UNUSED)
 
 void halt(void) {
   shutdown_power_off();
+}
+
+void exit (int status) {
+  return;
+}
+
+pid_t exec (const char *file) {
+  return 0;
+}
+
+int wait (pid_t) {
+  return 0;
+}
+
+bool create (const char *file, unsigned initial_size) {
+  return 0;
+}
+
+bool remove (const char *file) {
+  return 0;
+}
+
+int open (const char *file) {
+  return 0;  
+}
+
+int filesize (int fd) {
+  return 0;
+}
+
+int read (int fd, void *buffer, unsigned length) {
+  return 0;
+}
+
+int write (int fd, const void *buffer, unsigned length) {
+  return 0;
+}
+
+void seek (int fd, unsigned position) {
+  return;   
+}
+
+unsigned tell (int fd) {
+  return 0;
+}
+
+void close (int fd) {
+  return;
 }
