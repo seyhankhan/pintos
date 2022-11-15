2d7f197bdc5edb0dedfb3972e9fa7e036fef0b67
Wahbi <wahbi.said20@imperial.ac.uk>
2022-11-15 12:20:58 +0000

    Resolved some merge conflicts manually - First 4 tests pass

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -17,7 +17,14 @@
 
 static void syscall_handler (struct intr_frame *);
 void read_args(void* esp, int num_args, void **args);
+void try_acquiring_filesys(void);
+void try_releasing_filesys(void);
+
+//should we make argument to this function void* or const char *?
 void check_fp_valid(void *file);
+
+struct lock lock_filesys;
+
 
 void
 syscall_init (void) 
@@ -28,7 +35,11 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  int system_call_number = *(int*)(f->esp);
+    int system_call_number = *(int*)(f->esp);
+
+    // array to store arguements passed into the system call
+    // passed to relevant function too
+    void* args[3];
 
   // array to store arguements passed into the system call
   // passed to relevant function too
@@ -37,7 +48,7 @@ syscall_handler (struct intr_frame *f UNUSED)
   int *esp = f->esp;
 
   esp++;
-
+  // Getting the arguments should be done in a better way
   int arg1 = *esp;
   int arg2 = *(esp + 1);
   int arg3 = *(esp + 2);
@@ -50,8 +61,15 @@ syscall_handler (struct intr_frame *f UNUSED)
       // printf("Called Write\n");
       f->eax =write(arg1, (void * ) arg2, arg3);
     break;
+    // Need to properly deal with exiting
     case SYS_EXIT:
-      thread_exit();
+      exit(0);
+    break;
+    case SYS_REMOVE:
+      read_args(f->esp, 1, args);
+      try_acquiring_filesys();
+      f->eax = remove(*(char**)args[0]);
+      try_releasing_filesys();
     break;
   }
 
@@ -69,23 +87,33 @@ void exit (int status) {
 }
 
 pid_t exec (const char *file) {
-  return 0;
+  check_fp_valid((char *) file);
+
+  char *arg;
+  char *save_ptr;
+
+  int filename_length = strlen(file);
+  char filename_copy[filename_length];
+  strlcpy(filename_copy, file, filename_length);
+  arg = strtok_r(filename_copy, " ", &save_ptr);
+
+  pid_t pid = -1;
+
+  if (filesys_open(arg)) {
+    // implementation of process_execute in argument passing branch, not here.
+    // need to merge changes
+    pid = process_execute(file);
+  }
+  return pid;
 }
 
-int wait (pid_t  pid) {
+int wait (pid_t  pid UNUSED) {
   return 0;
 }
 
 bool create (const char *file, unsigned initial_size) {
 
-  //check if pointer to file passed in is valid
-  if ((file) || (!is_user_vaddr(file))) {
-    exit(-1);
-  }
-  if (!pagedir_get_page(thread_current()->pagedir, file)) {
-    free(file);
-    exit(-1);
-  }
+  check_fp_valid((char *) file);
 
   //check if file has no name
   if (!strcmp(file, "")) {
@@ -96,54 +124,52 @@ bool create (const char *file, unsigned initial_size) {
   return filesys_create(file, initial_size);
 }
 
-bool remove (const char *file) {
-  return 0;
+// reads num_args arguments from esp, stores in args
+void read_args(void* esp, int num_args, void **args) {
+  int i = 0;
+  void *sp = esp;
+  for (; i < num_args; i++) {
+    sp += 4;
+    if (!sp || !is_user_vaddr(sp)) {
+      exit(-1);
+    }
+    args[i] = sp;
+  }
 }
 
-int open (const char *file) {
+bool remove (const char *file) {
+  check_fp_valid((char *) file);
+
+  // check if filename is empty, in which case can't remove
+  if (!strcmp(file, "")) {
+    return false;
+  }
+  bool deleted_file = filesys_remove(file);
+  return deleted_file;
+}
+
+int open (const char *file UNUSED) {
   return 0;  
 }
 
-int filesize (int fd) {
+int filesize (int fd UNUSED) {
   return 0;
 }
 
-int read (int fd, void *buffer, unsigned length) {
+int read (int fd UNUSED, void *buffer UNUSED, unsigned length UNUSED) {
   return 0;
 }
 
-int write (int fd, const void *buffer, unsigned length) {
-  // If no bytes have been written return default of 0
-  int ret = 0;
-
-  if (fd == STDOUT_FILENO) {
-    // Writing to the console
-    // returns number of bytes written
-    /* TODO: Keep in mind edge case where number of bytes written
-    is less than size because some were not able to be written. 
-    */
-    putbuf(buffer, length);
-    ret = length;
-  } 
-  // TODO: Writing to an actual file
-  return ret;
-}
-
-void seek (int fd, unsigned position) {
+void seek (int fd UNUSED, unsigned position UNUSED) {
   return;   
 }
 
-unsigned tell (int fd) {
+unsigned tell (int fd UNUSED) {
   return 0;
 }
 
-void close (int fd) {
+void close (int fd UNUSED) {
   return;
-}
-
-
-void halt(void) {
-  shutdown_power_off();
 }
 
 int write (int fd, const void *buffer, unsigned length) {
@@ -163,26 +189,28 @@ int write (int fd, const void *buffer, unsigned length) {
   return ret;
 }
 
-//wait(pid) return process_wait(pid);
+//should we make argument to this function void* or const char *?
+void check_fp_valid(void* file) {
+  //check if pointer to file passed in is valid
+  if ((file) || (!is_user_vaddr(file))) {
+    exit(-1);
+  }
+  if (!pagedir_get_page(thread_current()->pagedir, file)) {
+    free(file);
+    exit(-1);
+  }
+}
+
+void try_acquiring_filesys() {
+  if (lock_filesys.holder != thread_current()) {
+    lock_acquire(&lock_filesys);
+  }
+}
+
+void try_releasing_filesys() {
+  if (lock_filesys.holder != thread_current()) {
+    lock_release(&lock_filesys);
+  }
+}
 
 
-// pid_t exec (const char *file) {
-//   check_fp_valid(file);
-
-//   char *arg;
-//   char *save_ptr;
-
-//   int filename_length = strlen(file);
-//   char filename_copy[filename_length];
-//   strlcpy(filename_copy, file, filename_length);
-//   arg = strtok_r(filename_copy, " ", &save_ptr);
-
-//   pid_t pid = -1;
-
-//   if (filesys_open(arg)) {
-//     // implementation of process_execute in argument passing branch, not here.
-//     // need to merge changes
-//     pid = process_execute(file);
-//   }
-//   return pid;
-// }
