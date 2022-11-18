42efb9f5d2a980b9e6ba482cea6b9802bb83916a
Thomas White <tjw20@ic.ac.uk>
2022-11-18 06:50:37 +0000

    Updated comments

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -4,17 +4,17 @@
 #include "threads/thread.h"
 #include "threads/synch.h"
 
-//created when a process creates a child. Only deallocated when both the parent and child terminate
+//Stores the status of a process, and synchronization structures to ensure thread safe access
 struct process_exit_status {
   //exit code, 
   int exit_code; //shared by parent and child
   int ref_count; //keeps track of parent and child being alive to determine when to allocate. shared by parent and child
   int child_pid; //used by parent to find correct process_exit_status among its children
-  bool loaded; //process loaded correctly? parent&child
-  //int parent_pid maybe neccesary
-  struct list_elem elem; //for parent threads list of process
-  struct semaphore sema;
-  struct lock lock;
+  bool loaded;   //stores whether the process loaded correctly. shared by parent and child
+  
+  struct list_elem elem; //for parent thread's list of process statuses
+  struct semaphore sema; //downed by process_wait and upped by process_exit to ensure a parent waits for its child
+  struct lock lock;      //used for thread safe access to the shared data
 
   //add a semaphore/lock to ensure thread safety when changing this data?
 };
@@ -24,6 +24,7 @@ int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
 
+//decrements the ref_count of exit_status and frees the memory if no more references exist
 void dec_ref_count(struct process_exit_status *exit_status);
 
 #endif /* userprog/process.h */
