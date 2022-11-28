0aaa4db701afec2d5a13ca841ec1919da5957b88
Krish Maha <krm221@aube.doc.ic.ac.uk>
2022-11-28 11:33:19 +0000

    Define function to initialise frame table with number of frames

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -1,1 +1,28 @@
-#include "frame.h"
+#include <stddef.h>
+
+#include "vm/frame.h"
+#include "threads/palloc.h"
+#include "threads/synch.h"
+
+static size_t curr_frames;
+static struct lock lock_on_frame_table;
+
+//array to store frame table
+static struct frame** frame_table;
+
+void initialise_frame(int num_frames) {
+    void* addr = 0;
+    curr_frames = 0;
+    frame_table = malloc(sizeof(struct frame*) * num_frames);
+
+    //while possible keep allocating frames to frame table
+    while (addr == palloc_get_page(PAL_USER)) {
+        frame_table[num_frames] = malloc(sizeof(struct frame));
+        frame_table[num_frames]->page = NULL;
+        frame_table[num_frames]->addr = addr;
+        lock_init(&frame_table[num_frames]->lock);
+        curr_frames++;
+    }
+    lock_init(&lock_on_frame_table);
+
+}
--- src/vm/frame.h
+++ src/vm/frame.h
@@ -5,6 +5,8 @@ struct frame {
     struct page *page;    // pointer to page (if any) occupying frame
     struct lock lock;     // allows synchronisation of frame processes 
     void* addr;           // frame's starting address
-} 
+}; 
+
+void initialise_frame(int num_frames);
 
 #endif
