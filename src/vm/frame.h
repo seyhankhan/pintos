35b3fe21040e8dfa14fbed5d626bf7c37ac3d955
krm221 <krm221@ic.ac.uk>
2022-12-01 17:47:52 +0000

    Recreate frame struct for hashtable

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -1,13 +1,16 @@
 #ifndef FRAME_H
 #define FRAME_H
 
+#include <hash.h>
 #include "threads/synch.h"
 #include "vm/page.h"
 
 struct frame {
-    struct page* page;    // pointer to page (if any) occupying frame
-    struct lock lock;     // allows synchronisation of frame processes 
-    void* addr;           // frame's starting address
+    struct page* page;          // pointer to page (if any) occupying frame
+    struct lock lock;           // allows synchronisation of frame processes 
+    void* addr;                 // frame page address
+    struct hash_elem hash_elem; // hash entry for frame table
+
 };
 
 
